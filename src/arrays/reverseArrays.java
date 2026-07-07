ef97fc8f04d11a19651c84fe333901355a176a53
raushank7460 <raushank7460@gmail.com>
2026-07-07 18:46:50 +0530

    add

---
+++ src/arrays/reverseArrays.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class reverseArrays {
+    public static void main(String[] args) {
+        int[]nums={1,2,3,4,5};
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        for(int x:nums){
+            System.out.print(x+" ");
+        }
+    }
+}
---
+++ src/strings/reverseString.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class reverseString {
+    public static void main(String[] args) {
+        String s="Raushan";
+        StringBuilder sb =new StringBuilder(s);
+      //  sb.reverse()
+        System.out.println(sb.reverse());
+
+    }
+}
