9a32979568335dd9135fa3a071145a43a4c69555
Raushan Kumar <raushank7460@gmail.com>
2026-07-14 19:40:35 +0530

    add

---
+++ src/Basics/simpleIntrest.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class simpleIntrest {
+    public static void main(String[] args) {
+        double p=400;
+        double r=3; //rate
+        double t=2 ; // time year
+
+        double res=(p*r*t)/100;
+        System.out.println(res);
+    }
+}
---
+++ src/Basics/userInput.java
@@ -0,0 +1,13 @@
+package Basics;
+
+import java.util.*;
+
+public class userInput {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter num :");
+        int num=100;
+        System.out.print(num);
+
+    }
+}
