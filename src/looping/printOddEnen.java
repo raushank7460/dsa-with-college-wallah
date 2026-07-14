11def2e0fecf56b4a0007d30f40df542fe511a94
Raushan Kumar <raushank7460@gmail.com>
2026-07-14 20:12:35 +0530

    add looping

---
+++ src/looping/printOddEnen.java
@@ -0,0 +1,14 @@
+package looping;
+
+public class printOddEnen {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+                System.out.println("Even no:"+i+",");
+
+            }else{
+                System.out.println("Odd No :"+i+",");
+            }
+        }
+    }
+}
